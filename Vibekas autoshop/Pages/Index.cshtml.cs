411e8a1fce19a709ec65284d6d6345c3010e759c
chr005-dev <chr005@edu.zealand.dk>
2026-09-03 12:22:54 +0200

    Add project files.

---
+++ Vibekas autoshop/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vibekas_autoshop.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
